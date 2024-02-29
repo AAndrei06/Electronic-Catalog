f8e3dabc70e70154cc9811531ef07195e1249295
AAndrei06 <andreiarsenii90@gmail.com>
2024-02-29 17:39:01 +0200

    Added PostgreSQL and Docker to the project configured everything for PgAdmin4

--- electronic_catalog/electronic_catalog/settings.py
+++ electronic_catalog/electronic_catalog/settings.py
@@ -75,10 +75,21 @@ WSGI_APPLICATION = 'electronic_catalog.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+#DATABASES = {
+#   'default': {
+#       'ENGINE': 'django.db.backends.sqlite3',
+#       'NAME': BASE_DIR / 'db.sqlite3',
+#   }
+#}
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    'default':{
+        'ENGINE':'django.db.backends.postgresql_psycopg2',
+        'NAME':'test_db',
+        'USER':'root',
+        'PASSWORD':'root',
+        'HOST':'127.0.0.1',
+        'PORT':'5432'
     }
 }
 
